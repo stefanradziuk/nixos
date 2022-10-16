8390a38da190c734e7c9bc9e9fc595a2d271db78
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2022-10-16 20:13:42 +0100

    fix nias disk path

--- hosts/nias/default.nix
+++ hosts/nias/default.nix
@@ -5,7 +5,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/B926-17D7";
+    device = "/dev/disk/by-uuid/B926-17D7";
     fsType = "vfat";
   };
 }
