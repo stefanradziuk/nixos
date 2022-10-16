bcd99e935b7668c0f0be979dba83aa2bacfec802
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2022-10-16 19:40:01 +0100

    move boot fs mount into host specific settings

--- hosts/ellesmere/default.nix
+++ hosts/ellesmere/default.nix
@@ -3,4 +3,9 @@
 {
   networking.hostName = "ellesmere";
   imports = [ ./hardware-configuration.nix ];
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
 }
--- hosts/nias/default.nix
+++ hosts/nias/default.nix
@@ -3,4 +3,9 @@
 {
   networking.hostName = "nias";
   imports = [ ./hardware-configuration.nix ];
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/B926-17D7";
+    fsType = "vfat";
+  };
 }
