73c2bc3993d4ef10e6a7ec0b8bca8c657a5dfccd
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2023-01-17 23:30:33 +0000

    set xdg mime for zathura

--- stefan.nix
+++ stefan.nix
@@ -160,4 +160,14 @@ in {
       ]
     );
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = ["org.pwmt.zathura.desktop"];
+    };
+    defaultApplications = {
+      "application/pdf" = ["org.pwmt.zathura.desktop"];
+    };
+  };
 }
