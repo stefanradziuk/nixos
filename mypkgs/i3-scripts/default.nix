5264be0f527adee7dc3dd6002fc6713aabcae760
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2023-02-10 23:47:15 +0000

    mypkgs.i3-scripts: setup for packaging py scripts, add i3-renumber-once

--- mypkgs/i3-scripts/default.nix
+++ mypkgs/i3-scripts/default.nix
@@ -1,9 +1,16 @@
-{ stdenv
+{ pkgs
+, stdenv
 }:
 
-stdenv.mkDerivation {
+let
+  python3 = pkgs.python3.withPackages (python-packages:
+  with python-packages; [ i3ipc ]
+  );
+
+in stdenv.mkDerivation {
   name = "i3-scripts";
   src = ./src;
+  propagatedBuildInputs = [ python3 ];
   installPhase = ''
     mkdir -p $out/bin/
     mv * $out/bin/
