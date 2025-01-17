9195d6b07967cf929ab6b1c8ad8babac632c258e
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2025-01-17 23:00:42 +0000

    update nvim configs

--- dots/.config/nvim/lua/gruvbox-config.lua
+++ dots/.config/nvim/lua/gruvbox-config.lua
@@ -1,16 +1,24 @@
 local contrast = "hard"
-local colors = require("gruvbox.palette").get_base_colors(vim.o.background, contrast)
+local colors = require("gruvbox").palette
 
 colors["dark0_hard"] = "#1d2021"
 colors["dark0"] = "#282828"
 colors["dark0_soft"] = "#32302f"
+
+-- see https://github.com/ellisonleao/gruvbox.nvim/issues/306
+local bg0 = colors.dark0_hard
 
 -- setup must be called before loading the colorscheme
 require("gruvbox").setup({
   undercurl = true,
   underline = true,
   bold = true,
-  italic = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
   strikethrough = true,
   invert_selection = false,
   invert_signs = false,
@@ -18,30 +26,37 @@ require("gruvbox").setup({
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
   contrast = contrast, -- can be "hard", "soft" or empty string
-  palette_overrides = {},
+  palette_overrides = {
+    dark0_hard = "#1d2021",
+    dark0 = "#282828",
+    dark0_soft = "#32302f",
+  },
   overrides = {
     CoqtailChecked    = {bg = colors.dark0_soft},
     CoqtailSent       = {bg = colors.neutral_purple},
 
-    GitGutterAdd      = {fg = colors.green},
-    GitGutterChange   = {fg = colors.yellow},
-    GitGutterDelete   = {fg = colors.red},
+    GitGutterAdd      = {fg = colors.neutral_green},
+    GitGutterChange   = {fg = colors.neutral_yellow},
+    GitGutterDelete   = {fg = colors.neutral_red},
 
-    GruvboxAquaSign   = {bg = colors.bg0},
-    GruvboxBlueSign   = {bg = colors.bg0},
-    GruvboxGreenSign  = {bg = colors.bg0},
-    GruvboxOrangeSign = {bg = colors.bg0},
-    GruvboxPurpleSign = {bg = colors.bg0},
-    GruvboxRedSign    = {bg = colors.bg0},
-    GruvboxYellowSign = {bg = colors.bg0},
+    GruvboxAquaSign   = {bg = bg0},
+    GruvboxBlueSign   = {bg = bg0},
+    GruvboxGreenSign  = {bg = bg0},
+    GruvboxOrangeSign = {bg = bg0},
+    GruvboxPurpleSign = {bg = bg0},
+    GruvboxRedSign    = {bg = bg0},
+    GruvboxYellowSign = {bg = bg0},
 
-    SignColumn        = {bg = colors.bg0},
-    StatusLine        = {fg = colors.bg0},
-    StatusLineNC      = {fg = colors.bg0},
-    TabLine           = {bg = colors.bg0},
-    TabLineFill       = {bg = colors.bg0},
-    TabLineSel        = {bg = colors.bg0},
-    Title             = {bg = colors.bg0},
+    LineNr            = {bg = bg0},
+    CursorLineNr      = {bg = bg0},
+
+    SignColumn        = {bg = bg0},
+    StatusLine        = {fg = bg0},
+    StatusLineNC      = {fg = bg0},
+    TabLine           = {bg = bg0},
+    TabLineFill       = {bg = bg0},
+    TabLineSel        = {bg = bg0},
+    Title             = {bg = bg0},
   },
   dim_inactive = false,
   transparent_mode = false,
--- dots/.config/nvim/lua/treesitter-config.lua
+++ dots/.config/nvim/lua/treesitter-config.lua
@@ -1,25 +1,40 @@
--- See https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
-local telescope = require("telescope")
-local telescopeConfig = require("telescope.config")
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "latex" },
 
--- Clone the default Telescope configuration
-local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
 
--- I want to search in hidden/dot files.
-table.insert(vimgrep_arguments, "--hidden")
--- I don't want to search in the `.git` directory.
-table.insert(vimgrep_arguments, "--glob")
-table.insert(vimgrep_arguments, "!**/.git/*")
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
 
-telescope.setup({
-  defaults = {
-    -- `hidden = true` is not supported in text grep commands.
-    vimgrep_arguments = vimgrep_arguments,
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
   },
-  pickers = {
-    find_files = {
-      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-    },
-  },
-})
+}
